38bbf8551c902da29b711528f3a614c834a4c385
Bello Oladipupo <bellooladipupo41@gmail.com>
2018-09-06 20:05:22 +0100

    Create encryptPasswordVersion model method

--- modelMethods/user/index.js
+++ modelMethods/user/index.js
@@ -1,11 +1,13 @@
 const sendActivationLink = require('./sendActivationLink');
 const sendPasswordResetLink = require('./sendPasswordResetLink');
 const signToken = require('./signToken');
+const encryptPasswordVersion = require('./encryptPasswordVersion');
 const decodeToken = require('./decodeToken');
 
 module.exports = {
   sendActivationLink,
   sendPasswordResetLink,
   signToken,
+  encryptPasswordVersion,
   decodeToken,
 };
