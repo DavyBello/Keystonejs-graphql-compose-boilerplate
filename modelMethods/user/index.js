e9a4a096630a426f3121b5e09407e15d6e800a1f
Bello Oladipupo <bellooladipupo41@gmail.com>
2018-10-18 11:15:01 +0100

    Restructure user model methods

--- modelMethods/user/index.js
+++ modelMethods/user/index.js
@@ -4,6 +4,10 @@ const signToken = require('./signToken');
 const upsertGoogleUser = require('./upsertGoogleUser');
 const encryptPasswordVersion = require('./encryptPasswordVersion');
 const decodeToken = require('./decodeToken');
+const signToken = require('./auth/signToken');
+const upsertGoogleUser = require('./auth/upsertGoogleUser');
+const encryptPasswordVersion = require('./auth/encryptPasswordVersion');
+const decodeToken = require('./auth/decodeToken');
 
 module.exports = {
   sendActivationLink,
