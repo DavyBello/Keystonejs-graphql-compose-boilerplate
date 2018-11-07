163c6bc9e3f4e2ecbf1e8974e35fb8daf08647b3
Bello Oladipupo <bellooladipupo41@gmail.com>
2018-11-07 15:22:19 +0100

    Add server helpers

--- __tests__/helper/index.js
+++ __tests__/helper/index.js
@@ -1,11 +1,19 @@
 // const * as loaders = require('../src/loader');
 const createRows = require('./createRows');
+
 const { 
   connectMongoose,
   clearDatabase,
   disconnectMongoose,
   clearDbAndRestartCounters,
 } = require('./mongooseHelpers');
+
+const {
+  startKeystoneServer,
+  startKeystoneServerWithSubscriptions,
+  stopKeystoneServer,
+} = require('./serverHelpers');
+
 const getMockedContext = require('./getMockedContext');
 
 module.exports = {
@@ -14,6 +22,9 @@ module.exports = {
   clearDatabase,
   disconnectMongoose,
   clearDbAndRestartCounters,
+  startKeystoneServer,
+  startKeystoneServerWithSubscriptions,
+  stopKeystoneServer,
   getContext: getMockedContext,
   // sanitizeTestObject,
   // keystone,
